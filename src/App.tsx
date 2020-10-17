58458c2202a6c2a0f5148e5a9b0ef197416aca2d
mr.huo <455801665@qq.com>
2020-10-17 14:12:27 +0800

    创建views目录

--- src/App.tsx
+++ src/App.tsx
@@ -6,64 +6,32 @@ import {
   HashRouter as Router,
   Switch,
   Route,
-  Link,
   Redirect
 } from 'react-router-dom';
-import Nav from './components/Nav';
-
-
-
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
-const Main = styled.div`
-  display: flex;
-  flex-grow: 1;
-  overflow: auto;
-`;
-
-
+import Tags from 'views/Tags';
+import Money from './views/Money';
+import Statistics from './views/Statistics';
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
-          <Switch>
-            <Route path="/tags">
-              <Tags/>
-            </Route>
-            <Route path="/money">
-              <Money/>
-            </Route>
-            <Route path="/statistics">
-              <Statistics/>
-            </Route>
-            <Redirect exact from="/" to="money"/>
-            <Route path="*">
-              <NoMatch/>
-            </Route>
-          </Switch>
-        </Main>
-        <Nav/>
-      </Wrapper>
+      <Switch>
+        <Route path="/tags">
+          <Tags/>
+        </Route>
+        <Route path="/money">
+          <Money/>
+        </Route>
+        <Route path="/statistics">
+          <Statistics/>
+        </Route>
+        <Redirect exact from="/" to="money"/>
+        <Route path="*">
+          <NoMatch/>
+        </Route>
+      </Switch>
     </Router>
   );
 }
-
-function Statistics() {
-  return <h2>统计</h2>;
-}
-
-function Tags() {
-  return <h2>标签</h2>;
-}
-
-function Money() {
-  return <h2>记账</h2>;
-}
-
 function NoMatch() {
   return (
     <div>错误，输入的地址不对，请重新输入</div>
