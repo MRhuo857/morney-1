77cd42a0a6751b7abfe79c9a9a65634747f207dc
mr.huo <455801665@qq.com>
2020-10-17 12:30:44 +0800

    抽离nav

--- src/App.tsx
+++ src/App.tsx
@@ -8,29 +8,19 @@ import {
   Link,
   Redirect
 } from 'react-router-dom';
+import Nav from './components/Nav';
 
 const Wrapper = styled.div`
-  border:1px solid red;
   height: 100vh;
   display: flex;
   flex-direction: column;
 `;
 const Main = styled.div`
-  border: 1px solid pink;
   display: flex;
   flex-grow: 1;
   overflow: auto;
 `;
-const Nav = styled.nav`
-  border: 1px solid green;
-    >ul{
-      display: flex;
-        >li{
-        width: 33.333%;
-        padding: 16px;
-        }
-  }
-`;
+
 
 function App() {
   return (
@@ -53,19 +43,7 @@ function App() {
             </Route>
           </Switch>
         </Main>
-        <Nav>
-          <ul>
-            <li>
-              <Link to="/tags">标签</Link>
-            </li>
-            <li>
-              <Link to="/money">记账</Link>
-            </li>
-            <li>
-              <Link to="/statistics">统计</Link>
-            </li>
-          </ul>
-        </Nav>
+        <Nav/>
       </Wrapper>
     </Router>
   );
