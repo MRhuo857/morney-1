c477db169cf4391b4c985e71ad4f6a9492c1f3ba
mr.huo <455801665@qq.com>
2020-10-18 12:08:15 +0800

    收入支出可切换

--- src/views/Money/CategorySection.tsx
+++ src/views/Money/CategorySection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const CategorySection = styled.section`
+const Wrapper = styled.section`
   font-size: 24px;
   > ul{
     display:flex;
@@ -14,14 +15,33 @@ const CategorySection = styled.section`
         content: '';
         display:block; 
         height: 3px;
-        background:#333;
+        background: #ff6600;
         position:absolute;
         bottom:0;
         width: 100%;
         left: 0;
       }
+      &.selected{
+      color: #F60;
+      }
     }
   }
 `;
 
+const CategorySection:React.FC=()=>{
+  const [category,setCategory]=useState('-')
+  return  (
+    <Wrapper>
+      <ul>
+        <li className={category==='-'?'selected':''}
+        onClick={()=>{setCategory('-')}}
+        >支出</li>
+        <li className={category==='+'?'selected':''}
+        onClick={()=>{setCategory('+')}}
+        >收入</li>
+      </ul>
+    </Wrapper>
+  )
+
+}
 export {CategorySection}
