c6bf8ac904f29b4fc2c4465099d6ca1d2f3bca9f
mr.huo <455801665@qq.com>
2020-10-18 13:22:08 +0800

    重构numberPad

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,122 +1,53 @@
-import styled from 'styled-components';
 import React, {useState} from 'react';
+import {Wrapper} from './NumberPadSection/Wrapper';
+import {generateOutPut} from './NumberPadSection/generateOutPut';
 
-const Wrapper = styled.section`
-  display:flex;
-  flex-direction: column;
-  > .output{
-    background:white;
-    font-size: 36px;
-    line-height: 72px;
-    text-align:right;
-    padding: 0 16px;
-    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
-                inset 0 5px 5px -5px rgba(0,0,0,0.25);
-  }
-  > .pad{ 
-    > button{
-      font-size: 18px; float: left; width: 25%; height: 64px; border: none;
-      &.ok{ height: 128px; float: right; }
-      &.zero{ width: 50%; }
-      &:nth-child(1){
-        background:#f2f2f2;
-      }
-      &:nth-child(2),
-      &:nth-child(5) {
-        background:#E0E0E0;
-      }
-      &:nth-child(3),
-      &:nth-child(6),
-      &:nth-child(9) {
-        background:#D3D3D3;
-      }
-      &:nth-child(4),
-      &:nth-child(7),
-      &:nth-child(10) {
-        background:#C1C1C1;
-      }
-      &:nth-child(8),
-      &:nth-child(11),
-      &:nth-child(13) {
-        background:#B8B8B8;
-      }
-      &:nth-child(12) {
-        background:#9A9A9A;
-      }
-      &:nth-child(14) {
-        background:#A9A9A9;
-      }
-    }
-  }
-`;
+
 const NumberPadSection = () => {
   const [outPut, _setOutPut] = useState('0');
-  const setOutPut=(outPut:string)=>{
-    if (outPut.length>16){
-      outPut=outPut.slice(0,16)
-    }else if (outPut.length===0){
-      outPut='0'
+  const setOutPut = (outPut: string) => {
+    if (outPut.length > 16) {
+      outPut = outPut.slice(0, 16);
+    } else if (outPut.length === 0) {
+      outPut = '0';
     }
-    _setOutPut(outPut)
-  }
+    _setOutPut(outPut);
+  };
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
     if (text === null) {return;}
-    switch (text) {
-      case'0':
-      case'1':
-      case'2':
-      case'3':
-      case'4':
-      case'5':
-      case'6':
-      case'7':
-      case'8':
-      case'9':
-        if (outPut==='0'){
-          setOutPut(text)
-        }else{
-          setOutPut(outPut+text)
-        }
-        break;
-      case '.':
-        if (outPut.indexOf('.')>=0){return;}
-        setOutPut(outPut+'.')
-        break;
-      case'清空':
-        setOutPut('0')
-        break
-      case '删除':
-        if (outPut.length===1){
-          setOutPut('0')
-        }else {
-          setOutPut(outPut.slice(0,-1))
-        }
+    if (text === 'ok') {
+      // ToDO
+      return;
+    }
+    if ('0123456789.'.split('').concat(['清空','删除']).indexOf(text)>=0){
+      setOutPut(generateOutPut(text,outPut))
     }
   };
-  return (
-    <Wrapper>
-      <div className="output">
-        {outPut}
-      </div>
-      <div className="pad clearfix" onClick={onClickButtonWrapper}>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>删除</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>清空</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button className="ok">OK</button>
-        <button className="zero">0</button>
-        <button className="dot">.</button>
-      </div>
-    </Wrapper>
-  );
-};
+return (
+  <Wrapper>
+    <div className="output">
+      {outPut}
+    </div>
+    <div className="pad clearfix" onClick={onClickButtonWrapper}>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>删除</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>清空</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button className="ok">OK</button>
+      <button className="zero">0</button>
+      <button className="dot">.</button>
+    </div>
+  </Wrapper>
+);
+}
+;
 
 export {NumberPadSection};
---
+++ src/views/Money/NumberPadSection/generateOutPut.tsx
@@ -0,0 +1,33 @@
+const generateOutPut = (text: string,outPut='0') => {
+  switch (text) {
+    case'0':
+    case'1':
+    case'2':
+    case'3':
+    case'4':
+    case'5':
+    case'6':
+    case'7':
+    case'8':
+    case'9':
+      if (outPut === '0') {
+        return text;
+      } else {
+        return outPut + text;
+      }
+    case '.':
+      if (outPut.indexOf('.') >= 0) {return outPut;}
+      return outPut + '.';
+    case'清空':
+      return '0';
+    case '删除':
+      if (outPut.length === 1) {
+        return '0';
+      } else {
+        return outPut.slice(0, -1);
+      }
+    default:
+      return ''
+  }
+};
+export {generateOutPut}
