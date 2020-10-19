d2f16e604456bbbaa4979f863f288d0e84b8133a
mr.huo <455801665@qq.com>
2020-10-19 22:34:38 +0800

    tag可回退

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
 
-// require('icons/money.svg');
-// require('icons/tags.svg');
-// require('icons/charts.svg');
+import React from 'react';
+import cs from 'classnames';
+
 let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
 try {importAll(require.context('icons', true, /\.svg$/));} catch (error) {console.log(error);}
 
-type Props={
-  name?:string
-}
+type Props = {
+  name?: string
+} & React.SVGAttributes<SVGElement>
 
-const Icon=(props:Props)=>{
+const Icon = (props: Props) => {
+  const {name, children, className, ...rest} = props
   return (
-    <svg className="icon">
-      {props.name && <use xlinkHref={`#`+props.name}/> }
+    <svg className={cs('icon', className)} {...rest}>
+      {props.name && <use xlinkHref={'#' + props.name}/>}
     </svg>
-  )
-}
+  );
+};
 
-export default Icon
+export default Icon;
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import {useTags} from '../useTags';
-import { useParams } from 'react-router-dom';
+import {useParams, useHistory} from 'react-router-dom';
 import Layout from '../components/Layout';
 import Icon from '../components/Icons';
 import {Button} from '../components/Button';
 import styled from 'styled-components';
 import {Input} from '../components/Input';
 import {Center} from '../components/Center';
-type Params={
-  id:string
+
+type Params = {
+  id: string
 }
 const InputWrapper = styled.div`
   background:white;
   padding: 0 16px;
   margin-top: 8px;
 `;
-const Topbar=styled.header`
+const Topbar = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -27,15 +28,15 @@ const Topbar=styled.header`
       height: 20px;
     }
 `;
-const Space=styled.div`
+const Space = styled.div`
   height: 40px;
-`
-const Tag:React.FC=(props)=>{
-  const {findTags, updateTag,deleteTag} = useTags();
+`;
+const Tag: React.FC = (props) => {
+  const {findTags, updateTag, deleteTag} = useTags();
   let {id: idString} = useParams<Params>();
   const tag = findTags(parseInt(idString));
-      const tagContent = (tag: { id: number; name: string }) => (
-      <div>
+  const tagContent = (tag: { id: number; name: string }) => (
+    <div>
       <div>
         <InputWrapper>
           <Input label="标签名" type="text" placeholder="标签名"
@@ -52,21 +53,24 @@ const Tag:React.FC=(props)=>{
           deleteTag(tag.id);
         }}>删除标签</Button>
       </Center>
-      </div>
-      );
+    </div>
+  );
+  // const history = useHistory();
+  const onClickBack = () => {
+   window.history.back()
+    console.log('hi')
+  };
+  return (
+    <Layout>
+      <Topbar>
+        <Icon name="left" onClick={onClickBack}/>
+        <span>编辑标签</span>
+        <Icon/>
+      </Topbar>
 
-      return (
-      <Layout>
-        <Topbar>
-          <Icon name="left"/>
-          <span>编辑标签</span>
-          <Icon/>
-        </Topbar>
-
-        {tag ? tagContent(tag) : <Center>tag 不存在</Center>}
+      {tag ? tagContent(tag) : <Center>tag 不存在</Center>}
     </Layout>
 
-
-  )
-}
-export {Tag}
+  );
+};
+export {Tag};
