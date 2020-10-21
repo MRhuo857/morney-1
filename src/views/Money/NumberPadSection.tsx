5213c67746f4628a4fff06ad26f4de19952f8e41
mr.huo <455801665@qq.com>
2020-10-21 22:07:02 +0800

    add record

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -2,58 +2,59 @@ import React from 'react';
 import {Wrapper} from './NumberPadSection/Wrapper';
 import {generateOutPut} from './NumberPadSection/generateOutPut';
 
-type Props={
-  value:number
-  onChange:(value:number)=>void
+type Props = {
+  value: number
+  onChange: (value: number) => void
+  onOk?: () => void
 }
-const NumberPadSection:React.FC<Props> = (props) => {
-  const outPut=props.value.toString()
-  const setOutPut = (outPut: string) => {
-    let value
-    if (outPut.length > 16) {
-      value = parseFloat(outPut.slice(0, 16)) ;
-    } else if (outPut.length === 0) {
-      value = 0;
-    }else {
-      value=parseFloat(outPut)
-    }
-    props.onChange(value);
-  };
-  const onClickButtonWrapper = (e: React.MouseEvent) => {
-    const text = (e.target as HTMLButtonElement).textContent;
-    if (text === null) {return;}
-    if (text === 'ok') {
-      // ToDO
-      return;
-    }
-    if ('0123456789.'.split('').concat(['清空','删除']).indexOf(text)>=0){
-      setOutPut(generateOutPut(text,outPut))
-    }
-  };
-return (
-  <Wrapper>
-    <div className="output">
-      {outPut}
-    </div>
-    <div className="pad clearfix" onClick={onClickButtonWrapper}>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>删除</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>清空</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button className="ok">OK</button>
-      <button className="zero">0</button>
-      <button className="dot">.</button>
-    </div>
-  </Wrapper>
-);
-}
+const NumberPadSection: React.FC<Props> = (props) => {
+    const outPut = props.value.toString();
+    const setOutPut = (outPut: string) => {
+      let value;
+      if (outPut.length > 16) {
+        value = parseFloat(outPut.slice(0, 16));
+      } else if (outPut.length === 0) {
+        value = 0;
+      } else {
+        value = parseFloat(outPut);
+      }
+      props.onChange(value);
+    };
+    const onClickButtonWrapper = (e: React.MouseEvent) => {
+      const text = (e.target as HTMLButtonElement).textContent;
+      if (text === null) {return;}
+      if (text === 'ok') {
+        if (props.onOk) {props.onOk();}
+        return;
+      }
+      if ('0123456789.'.split('').concat(['清空', '删除']).indexOf(text) >= 0) {
+        setOutPut(generateOutPut(text, outPut));
+      }
+    };
+    return (
+      <Wrapper>
+        <div className="output">
+          {outPut}
+        </div>
+        <div className="pad clearfix" onClick={onClickButtonWrapper}>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>删除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button className="ok">ok</button>
+          <button className="zero">0</button>
+          <button className="dot">.</button>
+        </div>
+      </Wrapper>
+    );
+  }
 ;
 
 export {NumberPadSection};
